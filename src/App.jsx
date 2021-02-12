0e13d353b68f55b69a77393802634a3ff0765d0e
maximVasileuski <maxim.vasileuski@gmail.com>
2021-02-12 18:02:54 +0300

    check ci/cd

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          CI/CD Works!
         </a>
       </header>
     </div>
