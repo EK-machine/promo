2daa1d0815e1d29a22156d8e2c747cfb944dd3db
EK-machine <57370574+EK-machine@users.noreply.github.com>
2021-02-17 12:41:25 +0300

    font awesome
    installed font awesome to the project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fab, faCheckSquare, faCoffee);
 
 function App() {
   return (
@@ -8,7 +13,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          <code>src/App.js</code>
+          and save to reload.
         </p>
         <a
           className="App-link"
@@ -18,7 +25,9 @@ function App() {
         >
           Learn React
         </a>
+
       </header>
+
     </div>
   );
 }
