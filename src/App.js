5508f37f52ed6b63e3e4674afe19c8cb4787f31b
EK-machine <57370574+EK-machine@users.noreply.github.com>
2021-02-17 13:03:27 +0300

    Styled components
    Installed styled components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import styled from 'styled-components';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        
       </header>
     </div>
   );
