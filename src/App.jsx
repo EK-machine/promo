ba25552beb10a955e447c2148d8c51548aa1f394
EK-machine <57370574+EK-machine@users.noreply.github.com>
2021-02-24 21:40:56 +0300

    Created and styled sidebar. It contains no logic.

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,11 @@
 import React from 'react';
 import './App.css';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
-// import logo from './logo.svg';
 import PageContainer from './pages/PagesContainer';
 import Sidebar from './sidebar/Sidebar';
-
-library.add(fab, faCheckSquare, faCoffee);
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          CI/CD Works!
-        </a>
-      </header> */}
       <Sidebar />
       <PageContainer />
     </div>
