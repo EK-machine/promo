d49338b974a438127a73a2a0029a0c74d6234b96
EK-machine <57370574+EK-machine@users.noreply.github.com>
2021-03-28 23:33:43 +0300

    started doing intersection observation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useIntersection } from 'react-use';
 import './header.css';
 import { faPhoneAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import {
@@ -9,66 +10,89 @@ import {
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Header() {
+  const headerRef = useRef(null);
+  const intersection = useIntersection(headerRef, {
+    root: null,
+    rootMargin: '0px 0px -500px 0px',
+    threshold: 1,
+  });
+
+  const showHeader = (element) => {
+    console.log(element.classList);
+  };
+
+  const hideHeader = (element) => {
+    console.log(element.classList);
+  };
+
+  if (intersection && intersection.intersectionRatio < 1) {
+    hideHeader('.header__opacity-container');
+  } else {
+    showHeader('.header__opacity-container');
+  }
+
   return (
-    <div className="header__container">
-      <div className="header__avatar-name-container">
-        <div className="header__avatar-container">
-          <img
-            className="header__avatar"
-            src="/images/avatar.jpg"
-            alt="EK face"
-          />
+    <div ref={headerRef} className="header__container">
+      <div className="header__opacity-container">
+        <div className="header__avatar-name-container">
+          <div className="header__avatar-container">
+            <img
+              className="header__avatar"
+              src="/images/avatar.jpg"
+              alt="EK face"
+            />
+          </div>
+          <div className="header__name-container">
+            <h1 className="header__name">Evgenij Kalabukhin</h1>
+          </div>
         </div>
-        <div className="header__name-container">
-          <h1 className="header__name">Evgenij Kalabukhin</h1>
+        <div className="header__contacts-container">
+          <ul className="header__contact-list">
+            <li className="header__contact-item">
+              <FontAwesomeIcon
+                className="header__contact-link_icon"
+                icon={faPhoneAlt}
+              />
+              <p className="header__contact-link_text">Phone</p>
+            </li>
+            <li className="header__contact-item">
+              <FontAwesomeIcon
+                className="header__contact-link_icon"
+                icon={faEnvelope}
+              />
+              <p className="header__contact-link_text">Mail</p>
+            </li>
+            <li className="header__contact-item">
+              <FontAwesomeIcon
+                className="header__contact-link_icon"
+                icon={faSkype}
+              />
+              <p className="header__contact-link_text">Skype</p>
+            </li>
+            <li className="header__contact-item">
+              <FontAwesomeIcon
+                className="header__contact-link_icon"
+                icon={faTelegramPlane}
+              />
+              <p className="header__contact-link_text">Telegram</p>
+            </li>
+            <li className="header__contact-item">
+              <FontAwesomeIcon
+                className="header__contact-link_icon"
+                icon={faLinkedinIn}
+              />
+              <p className="header__contact-link_text">Linkedin</p>
+            </li>
+          </ul>
         </div>
-      </div>
-      <div className="header__contacts-container">
-        <ul className="header__contact-list">
-          <li className="header__contact-item">
-            <FontAwesomeIcon
-              className="header__contact-link_icon"
-              icon={faPhoneAlt}
-            />
-            <p className="header__contact-link_text">Phone</p>
-          </li>
-          <li className="header__contact-item">
-            <FontAwesomeIcon
-              className="header__contact-link_icon"
-              icon={faEnvelope}
-            />
-            <p className="header__contact-link_text">Mail</p>
-          </li>
-          <li className="header__contact-item">
-            <FontAwesomeIcon
-              className="header__contact-link_icon"
-              icon={faSkype}
-            />
-            <p className="header__contact-link_text">Skype</p>
-          </li>
-          <li className="header__contact-item">
-            <FontAwesomeIcon
-              className="header__contact-link_icon"
-              icon={faTelegramPlane}
-            />
-            <p className="header__contact-link_text">Telegram</p>
-          </li>
-          <li className="header__contact-item">
-            <FontAwesomeIcon
-              className="header__contact-link_icon"
-              icon={faLinkedinIn}
-            />
-            <p className="header__contact-link_text">Linkedin</p>
-          </li>
-        </ul>
-      </div>
-      <div className="header__lang-container">
-        <button type="button" className="header__language-btn">
-          EN
-        </button>
-        <button type="button" className="header__language-btn">
-          RU
-        </button>
+        <div className="header__lang-container">
+          <button type="button" className="header__language-btn">
+            EN
+          </button>
+          <button type="button" className="header__language-btn">
+            RU
+          </button>
+        </div>
       </div>
     </div>
   );
