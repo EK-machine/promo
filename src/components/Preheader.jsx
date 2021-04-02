6dac9e841701445f9453a8ae9944b1c2acbc0e4b
EK-machine <57370574+EK-machine@users.noreply.github.com>
2021-04-02 22:30:06 +0300

    made preheader buttons layout

--- src/components/Preheader.jsx
+++ src/components/Preheader.jsx
@@ -2,13 +2,13 @@ import React, { useRef } from 'react';
 import classNames from 'classnames';
 import { useIntersection } from 'react-use';
 import './preheader.css';
-// import { faPhoneAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons';
-// import {
-//   faLinkedinIn,
-//   faTelegramPlane,
-//   faSkype,
-// } from '@fortawesome/free-brands-svg-icons';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhoneAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import {
+  faLinkedinIn,
+  faTelegramPlane,
+  faSkype,
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Preheader() {
   const preheaderRef = useRef(null);
@@ -31,7 +31,13 @@ function Preheader() {
   return (
     <div ref={preheaderRef} className="preheader__container">
       <div className={preheaderClasses}>
-        <section className="preheader__img-container" />
+        <section className="preheader__img-container">
+          <img
+            className="preheader__img"
+            src="/images/avatar.jpg"
+            alt="EK-face"
+          />
+        </section>
         <section className="preheader__contacts-container">
           <div className="preheader__bio-container">
             <h1 className="preheader__bio-name">
@@ -45,7 +51,74 @@ function Preheader() {
               Belarus
             </p>
           </div>
-          2
+          <div className="preheader__contacts-container_left">
+            <div className="preheader__contact-item">
+              <div className="preheader__contact-phone">
+                <a className="preheader__contact-link" href="#">
+                  <FontAwesomeIcon
+                    className="preheader__contact-link_icon"
+                    icon={faPhoneAlt}
+                  />
+                  <p className="preheader__contact-link_text">Phone</p>
+                </a>
+              </div>
+            </div>
+            <div className="preheader__contact-item" />
+            <div className="preheader__contact-item" />
+            <div className="preheader__contact-item" />
+            <div className="preheader__contact-item">
+              <div className="preheader__contact-mail">
+                <a className="preheader__contact-link" href="#">
+                  <FontAwesomeIcon
+                    className="preheader__contact-link_icon"
+                    icon={faEnvelope}
+                  />
+                  <p className="preheader__contact-link_text">Mail</p>
+                </a>
+              </div>
+            </div>
+            <div className="preheader__contact-item" />
+            <div className="preheader__contact-item" />
+            <div className="preheader__contact-item" />
+            <div className="preheader__contact-item">
+              <div className="preheader__contact-skype">
+                <a className="preheader__contact-link" href="#">
+                  <FontAwesomeIcon
+                    className="preheader__contact-link_icon"
+                    icon={faSkype}
+                  />
+                  <p className="preheader__contact-link_text">Skype</p>
+                </a>
+              </div>
+            </div>
+            <div className="preheader__contact-item" />
+            <div className="preheader__contact-item">
+              <div className="preheader__contact-telegram">
+                <a className="preheader__contact-link" href="#">
+                  <FontAwesomeIcon
+                    className="preheader__contact-link_icon"
+                    icon={faTelegramPlane}
+                  />
+                  <p className="preheader__contact-link_text">Telegram</p>
+                </a>
+              </div>
+            </div>
+            <div className="preheader__contact-item" />
+            <div className="preheader__contact-item">
+              <div className="preheader__contact-linkedin">
+                <a className="preheader__contact-link" href="#">
+                  <FontAwesomeIcon
+                    className="preheader__contact-link_icon"
+                    icon={faLinkedinIn}
+                  />
+                  <p className="preheader__contact-link_text">Linkedin</p>
+                </a>
+              </div>
+            </div>
+            <div className="preheader__contact-item" />
+            <div className="preheader__contact-item" />
+          </div>
+          <div className="preheader__contacts-container_right" />
         </section>
 
         {/* <section className="preheader__item preheader__img-container">
