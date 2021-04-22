196387f2e7d9deeb1165153e7afd20e2313c5a62
EK-machine <57370574+EK-machine@users.noreply.github.com>
2021-04-22 12:27:04 +0300

    changed header avatar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import './App.css';
 import PageContainer from './pages/PagesContainer';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <PageContainer />
-      </div>
+      <PageContainer />
     </BrowserRouter>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,50 +1,29 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import classNames from 'classnames';
 import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useIntersection } from 'react-use';
 import './header.css';
 import ContactButtons from './Contactbuttons';
 
 function Header() {
-  const headerRef = useRef(null);
-  const intersection = useIntersection(headerRef, {
-    root: null,
-    rootMargin: '0px 0px -400px 0px',
-    threshold: 0.7,
-  });
-
-  const isHeaderVisible = intersection && intersection.intersectionRatio > 0.7;
-
-  const headerAvatarClasses = isHeaderVisible
-    ? classNames('header__avatar')
-    : classNames('header__avatar_unvisible');
-
-  const headerNameClasses = isHeaderVisible
-    ? classNames('header__name')
-    : classNames('header__name_unvisible');
-
   return (
-    <div ref={headerRef} className="header__container">
+    <div className="header__container">
       <div className="header__avatar-name-container">
         <div className="header__avatar-container">
-          <img
-            className={headerAvatarClasses}
-            src="/images/avatar.jpg"
-            alt="EK face"
-          />
-          <div className="header__start-link_container">
-            <Link exact to="/" className="header__start-link">
-              <FontAwesomeIcon
-                className="header__start-link_icon"
-                icon={faChevronUp}
-              />
-            </Link>
-          </div>
+          <Link exact to="/" className="header__avatar-link">
+            <img
+              className="header__avatar"
+              src="/images/avatar.jpg"
+              alt="EK face"
+            />
+            <FontAwesomeIcon
+              className="header__avatar-link_icon"
+              icon={faChevronUp}
+            />
+          </Link>
         </div>
         <div className="header__name-container">
-          <h1 className={headerNameClasses}>
+          <h1 className="header__name">
             Evgenij <br /> Kalabukhin
           </h1>
         </div>
