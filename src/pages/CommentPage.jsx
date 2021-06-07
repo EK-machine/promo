bdc723f622516ef78415ae282f624761505246c2
EK-machine <57370574+EK-machine@users.noreply.github.com>
2021-06-07 23:00:28 +0300

    added loading spinner while fetching data

--- src/pages/CommentPage.jsx
+++ src/pages/CommentPage.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './commentpage.css';
+import BeatLoader from 'react-spinners/BeatLoader';
 import Comment from '../components/Comment';
 import reviewService from '../components/serverService.js';
 
 function CommentPage() {
-  const [isCommentLoaded, setIsCommentLoaded] = useState([]);
+  const [isCommentLoaded, setIsCommentLoaded] = useState(false);
   useEffect(() => {
     reviewService.getAll().then((result) => {
       setIsCommentLoaded(result);
@@ -18,17 +19,25 @@ function CommentPage() {
           <h1>Please leave a comment below</h1>
         </section>
         <section className="comment__section comment__comments-section">
-          {isCommentLoaded.map(
-            ({ summary, text, rating, viewOnly, username, id }) => (
-              <Comment
-                key={id}
-                summary={summary}
-                text={text}
-                rating={rating}
-                viewOnly={viewOnly}
-                username={username}
-              />
-            ),
+          {isCommentLoaded ? (
+            <div className="comment__comments-section_is-loaded">
+              {isCommentLoaded.map(
+                ({ summary, text, rating, viewOnly, username, id }) => (
+                  <Comment
+                    key={id}
+                    summary={summary}
+                    text={text}
+                    rating={rating}
+                    viewOnly={viewOnly}
+                    username={username}
+                  />
+                ),
+              )}
+            </div>
+          ) : (
+            <div className="comment__comments-section_is-loading">
+              <BeatLoader loading />
+            </div>
           )}
         </section>
       </div>
