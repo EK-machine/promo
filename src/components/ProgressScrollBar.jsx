2e81dbdf3e38a19e7701f838d2b0cf395c00c0a7
EK-machine <57370574+EK-machine@users.noreply.github.com>
2021-06-29 00:16:05 +0300

    connected styling og scrollBar and hire btn

--- src/components/ProgressScrollBar.jsx
+++ src/components/ProgressScrollBar.jsx
@@ -3,7 +3,11 @@ import './progressscrollbar.css';
 
 function ProgressScrollBar({ scroll }) {
   return (
-    <div className="bar__wrapper">
+    <div
+      className={scroll < 99 ? 'bar__wrapper' : 'bar__wrapper-unvisible'}
+
+      // className="bar__wrapper"
+    >
       <div className="bar__stripe" style={{ width: `${scroll}%` }} />
     </div>
   );
