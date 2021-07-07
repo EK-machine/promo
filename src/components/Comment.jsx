f5a9ec1a4db131033e4485fcce18aa23a9eed11c
EK-machine <57370574+EK-machine@users.noreply.github.com>
2021-07-07 10:30:07 +0300

    made truncated dext with a show/hide button

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,31 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './comment.css';
 import StarRate from './StarRate';
 import TruncatedText from './TruncatedText';
 
 function Comment({ summary, text, rating, viewOnly = true, username }) {
-  const [isShown, setIsShown] = useState(false);
-
-  const toggleText = () => setIsShown(!isShown);
-
   return (
     <div className="comment__item-container">
       <div className="comment__item_name-container">
         <p className="comment__item_name">{username}</p>
       </div>
       <div className="comment__item_title-container">
-        <TruncatedText summary={summary} visible={isShown} />
+        <TruncatedText summary={summary} />
       </div>
       <div className="comment__item_text-container">
-        <TruncatedText summary={text} visible={isShown} />
+        <TruncatedText summary={text} />
       </div>
-      <button
-        className="comment__show-text_btn"
-        onClick={toggleText}
-        type="button"
-      >
-        {isShown ? 'Hide text' : 'Show text'}
-      </button>
       <div className="comment__item_rating-container">
         <StarRate rating={rating} viewOnly={viewOnly} />
       </div>
--- src/components/TruncatedText.jsx
+++ src/components/TruncatedText.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './truncatedtext.css';
 
-function TruncatedText({ summary, visible }) {
-  return visible ? (
-    <p className="text__container-visible">{summary}</p>
-  ) : (
-    <p className="text__container-unvisible">{summary}</p>
-  );
+const NUM_OF_LETTERS = 130;
+
+function TruncatedText({ summary }) {
+  const [isShown, setIsShown] = useState(false);
+
+  const toggleText = () => setIsShown(!isShown);
+
+  return summary ? (
+    <div className="text__main-container">
+      {isShown ? (
+        <p className="text__container-visible">{summary}</p>
+      ) : (
+        <p className="text__container-unvisible">{summary}</p>
+      )}
+
+      {summary.length > NUM_OF_LETTERS ? (
+        <button
+          className="comment__show-text_btn"
+          onClick={toggleText}
+          type="button"
+        >
+          {isShown ? 'Hide text' : 'Show text'}
+        </button>
+      ) : null}
+    </div>
+  ) : null;
 }
 
 export default TruncatedText;
